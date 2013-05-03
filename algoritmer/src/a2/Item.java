d2b468d796e75907411cbb74f6d88d2da11c3d4b
Mathias Andreasen <ma222ce@student.lnu.se>
2013-05-03 21:28:32 +0200

    Added Item.java which implements the A2Item.java interface

---
+++ algoritmer/src/a2/Item.java
@@ -0,0 +1,41 @@
+package a2;
+
+public class Item implements A2Item {
+
+	@Override
+	public String getPerformer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getTransactionValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setPerformer(String name) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setTransactionValue(double value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setDate(String date) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
