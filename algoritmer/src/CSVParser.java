66f5dc068a212dac38bc225c45d102a51f5be7f6
Mathias Andreasen <ma222ce@student.lnu.se>
2013-05-04 10:28:49 +0200

    Added example CSV-file parser

---
+++ algoritmer/src/CSVParser.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+/**
+ * Test CSV parser, might be useful
+ * 
+ * @author mathias
+ *
+ */
+
+public class CSVParser {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader CSVFile =
+				new BufferedReader(new FileReader("/home/mathias/a2/input.csv"));
+		
+		String dataRow = CSVFile.readLine(); // read fist line.
+		
+		while(dataRow != null) {
+			String[] dataArray = dataRow.split(",");
+			for(String item:dataArray)
+				System.out.print(item + "\t");
+			System.out.println();
+			dataRow = CSVFile.readLine();
+		}
+		CSVFile.close();
+		System.out.println();
+	}
+
+}
